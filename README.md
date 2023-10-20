cab3438330c636c18deee7706e2fe3256602bef9
Yilliee <yilliee@proton.me>
2023-10-20 18:19:46 +0500

    timetable_parser: v2.0: Allow generating pdfs
    - Print example usage incase of invalid usage.
    - parse_timetable: Account for hidden columns ( if any ).
    - get_course_details: Keep track of which course is repeated.
    - merge_timetable_with_details:  Perform a fuzzy merge of both DFs.
    - generate_pdf : Use reportlab to generate a per-section based pdf of
                     the provided timetable

--- README.md
+++ README.md
@@ -1,35 +1,41 @@
 # FAST NUCES Timetable Parser
 
 This script processes a FAST NUCES timetable, extracting course
-details and lecture schedules. The extracted data is then
-outputted in CSV format.
+details and lecture schedules. The extracted data can be then
+outputted in CSV and/or PDF format.
 
 ## Dependencies
 
 This script requires that you have python installed on your
-system. It depends on openpyxl and pandas modules.
+system. It depends on openpyxl, pandas and reportlab module.
 
 To install the required dependencies:
 
 ```bash
-pip install openpyxl pandas
+pip install openpyxl pandas reportlab
 ```
 
 ## Usage
 
-Run the script with the following command:
+To get the exact syntax to run script with, use the following command:
 
 ```bash
-python3 timetable_parser.py [path_to_excel_file]
+python3 timetable_parser.py --help
 ```
 
-If no Excel file path is provided, the script will default to
-looking for a file named `timetable.xlsx` in the same
-directory.
+If `--excel_file` parameter is provided but no filename is provided next
+to it, the script will default to looking for a file named `timetable.xlsx`
+in the current working directory.
 
-The extracted data will be saved as a CSV file in the current
-working directory. The CSV filename will match the input
-Excel file's name.
+If the `--create_csv` parameter is passed, a copy of the extracted data will
+be saved as a CSV file in the current working directory. The CSV filename
+will default to `out.csv` unless overriden via the parameter:
+`--output_csv <csv_name>`.
+
+If the `--create_pdf` parameter is passed, a copy of the extracted data will
+be saved as a pdf file in the current working directory. The PDF filename
+will default to `out.pdf` unless overriden via the parameter:
+ `--output_pdf <pdf_name>`.
 
 ## Example
 
@@ -38,25 +44,26 @@ sample folder. Run the script for it with using the following
 command:
 
 ```bash
-python3 timetable_parser.py sample/FSC_Time_Table__List_of_Courses_Fall_2023_v1.1.xlsx
+python3 timetable_parser.py --excel_file sample/FSC_Time_Table__List_of_Courses_Fall_2023_v1.1.xlsx --create_pdf --output_pdf "FSC_TimeTable_Fall23_v1.1.pdf"
 ```
 
 The output file would be saved in the current directory with the
-name: `FSC_Time_Table__List_of_Courses_Fall_2023_v1.1.csv`
+name: `FSC_Timetable_Fall23_1.1.pdf`
 
 ## Features
 
-- Extracts course details and lecture schedules from the FAST
-  NUCES timetable.
-- Automatically matches course titles between timetable and
-  course details.
+- Extracts course details, lecture schedules and instructor names
+  from the FAST NUCES FSC timetable excel file.
+- Automatically links data from timetable sheet with that from the
+  list of courses sheets.
+- Allows to export the processed data as a pdf as well as a csv file.
 
 ## Compatibility
 
 Currently tested only on the FSC (FAST SCHOOL OF COMPUTING)
 timetable for Lahore campus ( Fall22, Spring23, Fall23 ).
 Other timetables should work as long as they follow a similar
-format.
+format with minor edits.
 
 ## Disclaimer
 
--- timetable_parser.py
+++ timetable_parser.py
@@ -1,12 +1,24 @@
 #!/bin/env python3
 
-import openpyxl
-from openpyxl.worksheet.worksheet import Worksheet as pyxl_Worksheet
-import openpyxl.cell.cell
+import sys
+import difflib
 import pandas as pd
 import pandas.core.series
-import difflib
-import sys
+import openpyxl as xl
+from openpyxl.utils import get_column_letter
+from openpyxl.worksheet.worksheet import Worksheet as xl_Worksheet
+from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.pagesizes import letter, landscape
+from reportlab.platypus import (
+    SimpleDocTemplate,
+    Table,
+    TableStyle,
+    Spacer,
+    Paragraph,
+    PageBreak,
+)
+from reportlab.platypus.flowables import KeepTogether
 
 
 def _get_day(curr_day: str, cache: dict = None) -> str:
@@ -34,7 +46,7 @@ def _get_day(curr_day: str, cache: dict = None) -> str:
     return ''
 
 
-def parse_timetable(sheet: pyxl_Worksheet) -> pd.DataFrame:
+def parse_timetable(sheet: xl_Worksheet) -> pd.DataFrame:
     """Extract course titles, sections, and lecture details from the main
       timetable sheet. Return the extracted data as a pandas dataframe."""
     total_columns = sheet.max_column
@@ -52,7 +64,7 @@ def parse_timetable(sheet: pyxl_Worksheet) -> pd.DataFrame:
 
     day = ''
 
-    starting_time = sheet[STARTING_ROW][2].value
+    starting_time = sheet[STARTING_ROW - 1][2].value
     if starting_time is None or ':' not in starting_time:
         hours_offset = 8
         minutes_offset = 30
@@ -74,7 +86,14 @@ def parse_timetable(sheet: pyxl_Worksheet) -> pd.DataFrame:
         room = room.strip()
 
         col_no = STARTING_COL
+        col_no_offset = 0
         while col_no < total_columns:
+            col_letter = get_column_letter(col_no + 1)
+            if sheet.column_dimensions[col_letter].hidden:
+                col_no += 1
+                col_no_offset += 1
+                continue
+
             cell = row[col_no]
             if cell.value is None:
                 col_no += cell_coordinates.get((cell.row, cell.column), 1)
@@ -89,13 +108,13 @@ def parse_timetable(sheet: pyxl_Worksheet) -> pd.DataFrame:
             # Ignore anything in brackets within the course name
             # Such things like are of no use to us
             title = course_details[0].split('(')[0].strip()
-            # Replace & with and for easier matching later on
+            # Replace '&' with 'and' for easier matching later on
             title = title.replace('&', 'and', 1)
 
             section_list = course_details[1].strip().rstrip(')').split(',')
 
-            start_time = [(hours_offset + (col_no - STARTING_COL) // 6),
-                          minutes_offset + ((col_no - STARTING_COL) % 6) * 10]
+            start_time = [(hours_offset + (col_no - col_no_offset - STARTING_COL) // 6),
+                          minutes_offset + ((col_no - col_no_offset - STARTING_COL) % 6) * 10]
             if start_time[1] >= 60:
                 start_time[0] += 1
                 start_time[1] -= 60
@@ -150,19 +169,8 @@ def parse_timetable(sheet: pyxl_Worksheet) -> pd.DataFrame:
     return pd.DataFrame()
 
 
-def _get_dept_from_course_code(course_code: str) -> str:
-    """Return the parent department corresponding to the course code.
-      Return an empty string ('') for unknown course codes."""
-    departments = {'NS': 'NS', ('MT', 'SS', 'SL'): 'HSS',
-                   ('CS', 'SE'): 'CS', 'DS': 'DS'}
-
-    if course_code is None:
-        return ''
-    return departments.get(course_code[:2], '')
-
-
-def get_course_details(workbook: openpyxl.Workbook,
-                       sheets_list: list[pyxl_Worksheet]) -> pd.DataFrame:
+def get_course_details(workbook: xl.Workbook,
+                       sheets_list: list[xl_Worksheet]) -> pd.DataFrame:
     """Extract the course details from all sheets in 'sheets_list' within the
       open workbook. Return a pandas dataframe containing the extracted
       data."""
@@ -218,16 +226,23 @@ def get_course_details(workbook: openpyxl.Workbook,
         if starting_row == sheet.max_row or starting_row == -1:
             break
 
+        repeated = False
         for row in sheet.iter_rows(min_row=starting_row, values_only=True):
             code, title = row[col_num['code']], row[col_num['title']]
-            if code is None or title is None:
-                continue
-            title = title.strip().replace('&', 'and', 1)
-            code = code.strip()
-
             section = row[col_num['section']]
-            if section is not None:
-                section.strip()
+            if title is None:
+                continue
+            if code is None or section is None:
+                if 'repeat' in title.lower():
+                    repeated = True
+                else:
+                    repeated = False
+                continue
+
+
+            title = title.split('(')[0].strip().replace('&', 'and', 1)
+            code = code.strip()
+            section = section.strip()
 
             # Skip duplicates if any
             if (title, section) in course_cache:
@@ -235,7 +250,6 @@ def get_course_details(workbook: openpyxl.Workbook,
             course_cache.add((title, section))
 
             course = {
-                # Replace & with and for easier matching later on,
                 'title': title,
                 'code': code,
                 'section': section,
@@ -285,6 +299,8 @@ def get_course_details(workbook: openpyxl.Workbook,
                 course['parent_department'] = parent_dept
                 course['type'] = course_type
 
+            course['repeat'] = repeated
+
             course_details.append(course)
 
     if len(course_details) > 0:
@@ -292,11 +308,172 @@ def get_course_details(workbook: openpyxl.Workbook,
     return pd.DataFrame()
 
 
+def merge_timetable_with_details(course_details: pd.DataFrame,
+                                 timetable: pd.DataFrame) -> pd.DataFrame:
+    # Match titles between the two dataframes
+    timetable['title'] = timetable.apply(
+       lambda row: _get_corresponding_title(row, course_details),
+       axis=1
+    )
+
+    course_data = course_details.merge(
+       timetable, on=['section', 'title'], how='right',
+       indicator=True
+    )
+    course_data['instructor'] = course_data['instructor'].fillna('');
+
+    # Seperate the right_only data for further processing
+    course_data_unmerged = course_data[
+        course_data['_merge'] == 'right_only'
+    ][timetable.columns]
+    # Convert course_data to a inner merged one
+    course_data = course_data[course_data['_merge'] == 'both']
+
+    # No use for merge column anymore
+    course_data.drop('_merge', axis=1, inplace=True)
+
+    # Is there even a need of further processing ?
+    if course_data_unmerged.empty:
+        return course_data
+
+    # Temporary column for secondary merge
+    course_details['sec_tmp'] = course_details['section'] \
+                                    .apply(lambda sec : sec[:6])
+    course_data_unmerged['sec_tmp'] = course_data_unmerged['section'] \
+                                        .apply(lambda sec: sec[:6])
+
+    # Drop temporary & overlapping columns
+    course_data_unmerged = course_data_unmerged.merge(
+        course_details, on=['sec_tmp', 'title'], how='left',
+        suffixes=('', '_details')
+    ).drop(['section_details', 'sec_tmp'], axis=1)
+    course_details = course_details.drop('sec_tmp', axis=1)
+    # Append to the end of old data to convert it back to original state
+    course_data = pd.concat(
+        [course_data, course_data_unmerged],
+        ignore_index=True
+    )
+
+    return course_data
+
+
+def generate_pdf(course_data: pd.DataFrame, input_filename: str,
+                 output_file: str) -> None:
+    doc = SimpleDocTemplate(output_file, pagesize=landscape(letter))
+
+    doc_elements = []
+
+    stylesheet = getSampleStyleSheet()
+    h1_style = stylesheet['Heading1']
+    h1_style.alignment = 1
+    h2_style = stylesheet['Heading2']
+    h2_style.alignment = 1 # Center alignment
+
+    # Extra columns to facilitate with grouping
+    course_data['dept_and_batch'] = course_data['section'].str[:5]
+    course_data['sections_group'] = course_data['section'].str[:6]
+
+    grouped_data = course_data.groupby(
+        ['type', 'dept_and_batch', 'repeat', 'sections_group']
+    )
+
+    for group_name, data in grouped_data:
+        course_type, _, repeat_status, section = group_name
+
+        doc_elements.append(Paragraph(f"{course_type.rstrip('s')} Course", h1_style))
+
+        if repeat_status:
+            doc_elements.append(Paragraph('(Repeated)', h2_style))
+
+        section_heading = Paragraph(f"<u>{section}</u>", h2_style)
+
+        table_data = []
+        table_header = [
+            'Day', 'Start Time', 'End Time', 'Subject', 'Room', 'Instructor', 'Credit Hours'
+        ]
+
+        for row in data.itertuples(index=False):
+            for lecture in row.lectures:
+                table_data.append([
+                    lecture['day'], lecture['start_time'], lecture['end_time'],
+                    row.title, lecture['room'], row.instructor, row.credit_hours
+                ])
+                # Append section letter + number to title of lectures of lab
+                section_prefix = row.section[6:]
+                if section_prefix != '':
+                    table_data[-1][3] += f' ({row.section[5:]})'
+
+        # Sort the table data by days and timings
+        table_data = sorted(table_data,
+            key=lambda x: (_get_day_no(x[0]), x[1])
+        )
+
+        # Merge consecutive cells with the same day value
+        prev_day = None
+        span_cells = []
+        for row_no, row in enumerate(table_data):
+            if row[0] == prev_day:
+                span_cells[-1][1][1] += 1
+                # Empty cells which'll be merged into others
+                row[0] = ''
+            else:
+                prev_day = row[0]
+                span_cells.append(
+                     ((0, row_no + 1), [0, row_no + 1])
+                 )
+        table_data.insert(0, table_header)
+
+        section_table = Table(
+            table_data,
+            splitByRow=1,
+            style=TableStyle([
+                    ("BACKGROUND", (0, 0), (-1, 0), colors.blue),
+                    ("TEXTCOLOR", (0, 0), (-1, 0), colors.white),
+                    ("FONTNAME", (0, 0), (-1, 0), "Helvetica-Bold"),
+                    ("BOTTOMPADDING", (0, 0), (-1, 0), 12),
+                    ("ALIGN", (0, 0), (-1, -1), "CENTER"),
+                    ("GRID", (0, 0), (-1, -1), 1, colors.black),
+                    ("BACKGROUND", (0, 1), (-1, 1), colors.white),
+                    ("ALIGN", (0, 1), (-1, 1), "CENTER"),
+                    ("VALIGN", (0, 0), (0, -1), "MIDDLE"),
+                ] + [
+                    ("SPAN", cell, (span_range[0], span_range[1])) \
+                    for cell, span_range in span_cells
+                ]
+            ),
+        )
+
+        table_with_header = KeepTogether([section_heading, section_table])
+
+        doc_elements.append(table_with_header)
+        doc_elements.append(PageBreak())
+
+    doc_elements.append(
+        Paragraph(f"Generated from {input_filename.split('/',)[-1]}.", h1_style)
+    )
+    doc_elements.append(Spacer(1, 20))
+    doc_elements.append(
+        Paragraph("For any issues/feedback, contact at : yilliee@proton.me", h2_style)
+    )
+
+    doc.build(doc_elements)
+
+
+def _get_dept_from_course_code(course_code: str) -> str:
+    """Return the parent department corresponding to the course code.
+      Return an empty string ('') for unknown course codes."""
+    departments = {'NS': 'NS', 'MT': 'HSS', 'MT': 'HSS', 'MT':'HSS',
+                   'CS' : 'CS', 'SE': 'CS', 'DS': 'DS'}
+
+    if course_code is None:
+        return ''
+    return departments.get(course_code[:2], '')
+
+
 def _get_corresponding_title(row: pandas.core.series.Series,
                              details_df: pd.DataFrame) -> str:
     """Return the closest matching title for the current row from the
       details DataFrame."""
-
     title = row['title']
     section = row['section']
 
@@ -323,25 +500,83 @@ def _get_corresponding_title(row: pandas.core.series.Series,
 
     res = difflib.get_close_matches(title, details_df['title'])
 
-    return res[0] if len(res) > 0 else None
+    return res[0] if len(res) > 0 else title
+
+
+def _get_day_no(day: str) -> int:
+    """Returns the chronological number of the day in a week"""
+    days = {
+        'monday': 1,
+        'tuesday': 2,
+        'wednesday': 3,
+        'thursday': 4,
+        'friday': 5,
+        'saturday': 6,
+        'sunday': 7
+    }
+
+    return days.get(day.lower(), 0)
+
+
+def _print_example_usage():
+    print("Usage:")
+    print(
+    f"""\t{sys.argv[0]}
+        \t--excel_file <filename>
+        \t[--create_csv [--output_csv <filename>]]
+        \t[--create_pdf [--output_pdf <filename>] """
+    )
+
+    print("Example:")
+    print(f"\t{sys.argv[0]} "
+          "--excel_file 'sample/FSC_Time_Table__List_of_Courses_Fall_2023_v1.1.xlsx'")
+    print(f"\t{sys.argv[0]} "
+          "--excel_file 'sample/FSC_Time_Table__List_of_Courses_Fall_2023_v1.1.xlsx' "
+          "--create_csv --output_csv 'sample_Fall23_v1.1.csv' "
+          "--create_pdf --output_pdf 'sample_Fall23_v1.1.pdf'"
+    )
 
 
 def main():
-    # Filename of Excel file
+    if '--help' in sys.argv:
+        _print_example_usage()
+        sys.exit(0)
+
     try:
-        filename = sys.argv[1]
-    except IndexError:
+        filename = sys.argv[sys.argv.index('--excel_file') + 1]
+        # Do we actually have a proper filename ?
+        if filename[0] == '-':
+            raise ValueError
+    except (ValueError, IndexError):
         filename = 'timetable.xlsx'
 
-    output_csv_filename = f"{filename.split('/')[-1].strip('.xlsx')}.csv"
+    create_csv = '--create_csv' in sys.argv
+    if create_csv:
+        try:
+            output_csv_filename = sys.argv[sys.argv.index('--output_csv') + 1]
+            # Do we actually have a proper filename ?
+            if output_csv_filename[0] == '-':
+                raise ValueError
+        except (ValueError, IndexError):
+            output_csv_filename = 'out.csv'
+
+    create_pdf = '--create_pdf' in sys.argv
+    if create_pdf:
+        try:
+            output_pdf_filename = sys.argv[sys.argv.index('--output_pdf') + 1]
+            # Do we actually have a proper filename ?
+            if output_pdf_filename[0] == '-':
+                raise ValueError
+        except (ValueError, IndexError):
+            output_pdf_filename = 'out.pdf'
 
     # Load the Excel file
     print(f'Attempting to open {filename}')
     try:
-        workbook = openpyxl.load_workbook(filename)
+        workbook = xl.load_workbook(filename)
     except FileNotFoundError:
         sys.stderr.write(f'Error : Unable to open file: {filename}.\n')
-        # _print_example_usage()
+        _print_example_usage()
         sys.exit(2)
 
     print(f'Successfully opened {filename}')
@@ -368,19 +603,22 @@ def main():
 
     # Update timetable's course titles to match those in course details
     print('\nMerging course and class details...')
-    course_timetable['title'] = course_timetable.apply(
-       lambda row: _get_corresponding_title(row, course_details),
-       axis=1
-    )
-    course_data = course_details.merge(
-       course_timetable, on=['section', 'title'], how='right'
+    course_data = merge_timetable_with_details(
+        course_details=course_details,
+        timetable=course_timetable,
     )
     print('Done.')
 
-    print(f'\nExporting to {output_csv_filename}')
-    with open(output_csv_filename, 'w') as file:
-        course_data.to_csv(file)
-    print('Done')
+    if create_csv:
+        print(f'\nExporting to {output_csv_filename}')
+        with open(output_csv_filename, 'w') as file:
+            course_data.to_csv(file)
+        print('Done')
+
+    if create_pdf:
+        print(f'\nGenerating {output_pdf_filename}')
+        generate_pdf(course_data, filename, output_pdf_filename)
+        print('Done')
 
 
 if __name__ == "__main__":
